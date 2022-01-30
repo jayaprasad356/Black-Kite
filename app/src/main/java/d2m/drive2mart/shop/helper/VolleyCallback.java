239dde65e70e25dca65d6344ccf42d3efd5da080
jayaprasad356 <jayaprasad356@gmail.com>
2022-01-30 15:41:53 +0530

    black kite commit

--- app/src/main/java/d2m/drive2mart/shop/helper/VolleyCallback.java
+++
@@ -1,7 +0,0 @@
-package d2m.drive2mart.shop.helper;
-
-public interface VolleyCallback {
-    void onSuccess(boolean result, String message);
-    //void onSuccessWithMsg(boolean result, String message);
-
-}
